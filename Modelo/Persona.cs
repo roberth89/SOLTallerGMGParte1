eaa99e7cb8f21156832c447d1a66f26c0a4610e6
Roberto Cordero Quirós <RC4691CP@grupomonge.com>
2022-07-14 10:26:22 -0600

    se agrega libreria de clases modelo, y la clase persona.

---
+++ Modelo/Persona.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modelo
+{
+    public class Persona
+    {
+        public string Identificacion { get; set; }
+        public string Nombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public DateTime FechaTranjsaccion { get; set; }
+        public decimal MontoTransaccion { get; set; }
+        public string Mensaje { get; set; }
+
+    }
+}
