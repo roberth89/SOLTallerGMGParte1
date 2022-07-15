00ee08a6a48e37dc19b3dcdbb20c30f4c233cff8
Roberto Cordero Quirós <RC4691CP@grupomonge.com>
2022-07-15 09:52:40 -0600

    se agrega clase generica , de ejemplo para poder agregar colecciones

---
+++ LogicaNegocio/AlmacenaObjetoGenerico.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicaNegocio
+{
+    public class AlmacenaObjetoGenerico<T>
+    {
+        private readonly T[] _datosElemento;
+        private int _contador = 0;
+
+        public AlmacenaObjetoGenerico(int tamanno)
+        {
+            _datosElemento = new T[tamanno];
+        }
+
+        public void Agregar(T obj)
+        {
+            _datosElemento[_contador] = obj;
+            _contador++;
+        }
+
+        public T GetElemento(int posicion)
+        {
+            return _datosElemento[posicion];
+        }
+
+    }
+}
