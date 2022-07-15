ba121dcb9413bee4ca2b7b3c991235cb23cc9430
Roberto Cordero Quirós <RC4691CP@grupomonge.com>
2022-07-15 09:46:19 -0600

    se crea clase de ejemplo de generticos.

---
+++ LogicaNegocio/AlmacenaObjetoNormal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicaNegocio
+{
+    public class AlmacenaObjetoNormal
+    {
+        private readonly Object[] _datosElemento;
+        private int _contador = 0;
+
+        public AlmacenaObjetoNormal(int tamanno)
+        {
+            _datosElemento = new Object[tamanno];
+        }
+
+        public void Agregar(Object obj)
+        {
+            _datosElemento[_contador] = obj;
+            _contador++;
+        }
+
+        public Object GetElemento(int posicion)
+        {
+            return _datosElemento[posicion];
+        }
+
+    }
+}
